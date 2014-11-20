67a5c366ae16146fdb30e8c3d7ff70a3616b5c37
Reegan Layzell <roadysix@gmail.com>
2014-11-20 20:41:46 +0000

    Update new methods and static properties

--- Source/Myo.Net/Myo.Net/Include/Vector3.hpp
+++ Source/Myo.Net/Myo.Net/Include/Vector3.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+using namespace System;
+using namespace System::Globalization;
+using namespace System::Diagnostics;
+using namespace System::Runtime::InteropServices;
+using namespace System::Text;
+
 namespace MyoNet
 {
 	namespace Myo
@@ -7,20 +13,35 @@ namespace MyoNet
 		///<summary>
 		/// A vector of three components.
 		///</summary>
-		[System::Diagnostics::DebuggerDisplayAttribute("\\{X={X}, Y={Y}, Z={Z}\\}")]
-		[System::Runtime::InteropServices::StructLayoutAttribute(System::Runtime::InteropServices::LayoutKind::Explicit)]
-		public value struct Vector3 sealed
+		[DebuggerDisplayAttribute("{ToString( )}")]
+		[StructLayoutAttribute(LayoutKind::Explicit)]
+		public value struct Vector3 sealed : IEquatable<Vector3>, IFormattable
 		{
 		private:
-			[System::Runtime::InteropServices::FieldOffsetAttribute(0) ] double _x; 
-			[System::Runtime::InteropServices::FieldOffsetAttribute(8) ] double _y;
-			[System::Runtime::InteropServices::FieldOffsetAttribute(16)] double _z;
+			[FieldOffsetAttribute(0) ] double _x; 
+			[FieldOffsetAttribute(8) ] double _y;
+			[FieldOffsetAttribute(16)] double _z;
 
 		public:
 			///<summary>
-			/// Represents a 3-dimension vector that has X, Y, and Z values set to zero.
+			/// Represents a vector that has X, Y, and Z values set to zero.
 			///</summary>
-			static initonly Vector3 Zero = Vector3(0, 0, 0);
+			static property Vector3 Zero { Vector3 get( ) { return Vector3(0, 0, 0); } }
+			
+			///<summary>
+			/// Represents a vector that has X value set to 1 and the Y, Z values set to zero.
+			///</summary>
+			static property Vector3 UnitX { Vector3 get( ) { return Vector3(1, 0, 0); } }
+			
+			///<summary>
+			/// Represents a vector that has Y value set to one and the X, Z values set to zero.
+			///</summary>
+			static property Vector3 UnitY { Vector3 get( ) { return Vector3(0, 1, 0); } }
+			
+			///<summary>
+			/// Represents a vector that has Z value set to one and the X, Y values set to zero.
+			///</summary>
+			static property Vector3 UnitZ { Vector3 get( ) { return Vector3(0, 0, 1); } }
 
 			///<summary>
 			/// Initializes a new instance of the <see cref="Vector3"/> with the specified components.
@@ -83,27 +104,27 @@ namespace MyoNet
 			///<summary>
 			/// Adds the specified <see cref="Vector3"/>'s.
 			///</summary>
-			static Vector3 operator + (Vector3 lhs, Vector3 rhs);
+			static Vector3 operator + (Vector3^ lhs, Vector3^ rhs);
 
 			///<summary>
 			/// Subtracts a <see cref="Vector3"/> from another.
 			///</summary>
-			static Vector3 operator - (Vector3 lhs, Vector3 rhs);
+			static Vector3 operator - (Vector3^ lhs, Vector3^ rhs);
 
 			///<summary>
 			/// Multiplies the <see cref="Vector3"/> by specified scalar <paramref name="value"/>
 			///</summary>
-			static Vector3 operator * (Vector3 lhs, double value);
+			static Vector3 operator * (Vector3^ lhs, double value);
 			
 			///<summary>
 			/// Multiplies the <see cref="Vector3"/> by specified scalar <paramref name="value"/>
 			///</summary>
-			static Vector3 operator * (double value, Vector3 rhs);
+			static Vector3 operator * (double value, Vector3^ rhs);
 
 			///<summary>
 			/// Divides the <see cref="Vector3"/> by specified scalar <paramref name="value"/>
 			///</summary>
-			static Vector3 operator / (Vector3 lhs, double value);
+			static Vector3 operator / (Vector3^ lhs, double value);
 
 			///<summary>
 			/// Return a new unit <see cref="Vector3"/> corresponding to the same direction as this one.
@@ -115,19 +136,59 @@ namespace MyoNet
 			/// Calculates the dot product of the <see cref="Vector3"/>'s <paramref name="lhs"/> and <paramref name="rhs"/>.
 			///</summary>
 			///<returns>The dot product.</returns>
-			static double DotProduct(Vector3 lhs, Vector3 rhs);
+			static double DotProduct(Vector3^ lhs, Vector3^ rhs);
 
 			///<summary>
 			/// Calculates the cross product of the <see cref="Vector3"/>'s <paramref name="lhs"/> and <paramref name="rhs"/>.
 			///</summary>
 			///<returns>A new vector representing the cross product.</returns>
-			static Vector3 CrossProduct(Vector3 lhs, Vector3 rhs);
+			static Vector3 CrossProduct(Vector3^ lhs, Vector3^ rhs);
 
 			///<summary>
 			/// Calculates the angle between the <see cref="Vector3"/>'s <paramref name="lhs"/> and <paramref name="rhs"/>.
 			///</summary>
 			///<returns>The angle in radians.</returns>
-			static double AngleTo(Vector3 lhs, Vector3 rhs);
+			static double AngleTo(Vector3^ lhs, Vector3^ rhs);
+
+			bool Equals(Object^ obj) override
+			{
+				if (dynamic_cast<Vector3^>(obj) == nullptr)
+					return false;
+				return Equals(static_cast<Vector3>(obj));
+			}
+
+			virtual bool Equals(Vector3 other)
+			{
+				return this->X == other.X
+					&& this->Y == other.Y
+					&& this->Z == other.Z;
+			}
+
+			virtual String^ ToString( ) override
+			{
+				return ToString("G", CultureInfo::CurrentCulture);
+			}
+
+			String^ ToString(String^ format)
+			{
+				return ToString(format, CultureInfo::CurrentCulture);
+			}
+
+			virtual String^ ToString(String^ format, IFormatProvider^ formatProvider)
+			{
+				StringBuilder^ sb = gcnew StringBuilder( );
+				String^ sep = NumberFormatInfo::GetInstance(formatProvider)->NumberGroupSeparator + " ";
+
+				sb->Append("<");
+				sb->Append(((IFormattable^)this->X)->ToString(format, formatProvider));
+				sb->Append(sep);
+				sb->Append(((IFormattable^)this->Y)->ToString(format, formatProvider));
+				sb->Append(sep);
+				sb->Append(((IFormattable^)this->Z)->ToString(format, formatProvider));
+				sb->Append(">");
+
+				return sb->ToString( );
+			}
 		};
 	}
 }
--- Source/Myo.Net/Myo.Net/Source/Vector3.cpp
+++ Source/Myo.Net/Myo.Net/Source/Vector3.cpp
@@ -14,44 +14,44 @@ namespace MyoNet
 
 		}
 
-		Vector3 Vector3::operator + (Vector3 lhs, Vector3 rhs)
+		Vector3 Vector3::operator + (Vector3^ lhs, Vector3^ rhs)
 		{
 			return Vector3(
-				lhs.X + rhs.X,
-				lhs.Y + rhs.Y,
-				lhs.Z + rhs.Z);
+				lhs->X + rhs->X,
+				lhs->Y + rhs->Y,
+				lhs->Z + rhs->Z);
 		}
 
-		Vector3 Vector3::operator - (Vector3 lhs, Vector3 rhs)
+		Vector3 Vector3::operator - (Vector3^ lhs, Vector3^ rhs)
 		{
 			return Vector3(
-				lhs.X - rhs.X,
-				lhs.Y - rhs.Y,
-				lhs.Z - rhs.Z);
+				lhs->X - rhs->X,
+				lhs->Y - rhs->Y,
+				lhs->Z - rhs->Z);
 		}
 
-		Vector3 Vector3::operator * (Vector3 lhs, double value)
+		Vector3 Vector3::operator * (Vector3^ lhs, double value)
 		{
 			return Vector3(
-				lhs.X * value,
-				lhs.Y * value,
-				lhs.Z * value);
+				lhs->X * value,
+				lhs->Y * value,
+				lhs->Z * value);
 		}
 
-		Vector3 Vector3::operator * (double value, Vector3 rhs)
+		Vector3 Vector3::operator * (double value, Vector3^ rhs)
 		{
 			return Vector3(
-				rhs.X * value,
-				rhs.Y * value,
-				rhs.Z * value);
+				rhs->X * value,
+				rhs->Y * value,
+				rhs->Z * value);
 		}
 
-		Vector3 Vector3::operator / (Vector3 lhs, double value)
+		Vector3 Vector3::operator / (Vector3^ lhs, double value)
 		{
 			return Vector3(
-				lhs.X / value,
-				lhs.Y / value,
-				lhs.Z / value);
+				lhs->X / value,
+				lhs->Y / value,
+				lhs->Z / value);
 		}
 
 		Vector3 Vector3::Normalized( )
@@ -59,25 +59,25 @@ namespace MyoNet
 			return *this / this->Length( );
 		}
 
-		double Vector3::DotProduct(Vector3 lhs, Vector3 rhs)
+		double Vector3::DotProduct(Vector3^ lhs, Vector3^ rhs)
 		{
 			return 
-				lhs.X * rhs.X + 
-				lhs.Y * rhs.Y + 
-				lhs.Z * rhs.Z;
+				lhs->X * rhs->X + 
+				lhs->Y * rhs->Y + 
+				lhs->Z * rhs->Z;
 		}
 
-		Vector3 Vector3::CrossProduct(Vector3 lhs, Vector3 rhs)
+		Vector3 Vector3::CrossProduct(Vector3^ lhs, Vector3^ rhs)
 		{
 			return Vector3(
-				lhs.Y * rhs.Z - lhs.Z * rhs.Y,
-				lhs.Z * rhs.X - lhs.X * rhs.Z,
-				lhs.X * rhs.Y - lhs.Y * rhs.X);
+				lhs->Y * rhs->Z - lhs->Z * rhs->Y,
+				lhs->Z * rhs->X - lhs->X * rhs->Z,
+				lhs->X * rhs->Y - lhs->Y * rhs->X);
 		}
 
-		double Vector3::AngleTo(Vector3 lhs, Vector3 rhs)
+		double Vector3::AngleTo(Vector3^ lhs, Vector3^ rhs)
 		{
-			return System::Math::Acos(DotProduct(lhs, rhs) / (lhs.Length( ) * rhs.Length( )));
+			return System::Math::Acos(DotProduct(lhs, rhs) / (lhs->Length( ) * rhs->Length( )));
 		}
 	}
 }
